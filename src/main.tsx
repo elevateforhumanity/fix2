927d3014347a28c0f4fae1cbf0d9b1c9e8bf65a5
elevateforhumanity <elevateforhumanity@gmail.com>
2025-11-11 12:56:34 +0000

    Fix: Add Tailwind CSS import - styling now works

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { AuthProvider } from './contexts/AuthContext'
 import App from './App'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
