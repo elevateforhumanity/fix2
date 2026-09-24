d0fd2d167806162b3b9e8dd143680da866b5c29f
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-23 21:38:05 -0400

    Fix Elevate canonical NAP address

--- lib/contact-info.ts
+++ lib/contact-info.ts
@@ -17,11 +17,11 @@ export const CONTACT_INFO = {
 
   // Address
   address: {
-    street: "8888 Keystone Crossing Suite 1300",
+    street: "120 E Market St, Suite 930",
     city: "Indianapolis",
     state: "IN",
-    zip: "46240",
-    full: "8888 Keystone Crossing Suite 1300, Indianapolis, IN 46240",
+    zip: "46204",
+    full: "120 E Market St, Suite 930, Indianapolis, IN 46204",
   },
 
   // Hours
